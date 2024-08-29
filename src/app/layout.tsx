b21937959b6b97bc2b769d9751056ef4ed45a9cf
Waqar ahmed <waqarahmed44870@gmail.com>
2024-08-30 00:01:59 +0500

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,24 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter, Calistoga } from  "next/font/google"
 
 export const metadata: Metadata = {
   title: "My Portfolio",
   description: "Created with the help of Frontend Tribe",
 };
 
+const inter =  Inter({ subsets: [ "latin"], variable:"--font-sans" })
+const calistoga = Calistoga({ subsets: [ "latin"], variable:"--sans-serif", weight: ["400"] })
+
+
 export default function RootLayout({
-  children,
+ children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-gray-900 text-white antialiased ">{children}</body>
     </html>
   );
 }
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,12 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+  return <div>
+     <nav className="">
+      
+         <a href="/">Home</a>
+         <a href="/about">About</a>
+         <a href="/contact">Contact</a>
+         <a href="/tape">Tape</a>
+      
+     </nav>
+  </div>;
 };
