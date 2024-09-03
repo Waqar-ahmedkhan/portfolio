fbb30631aaab882bd25d9a78aa208099eb1fc051
Waqar ahmed <waqarahmed44870@gmail.com>
2024-09-03 17:07:39 +0500

    commit

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,6 @@
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return <div>Footer Section
+
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, quas! Distinctio, pariatur delectus. Totam laborum, dolores aliquid pariatur autem ipsum quam asperiores voluptas quia perferendis aspernatur, suscipit, provident ipsa nostrum.
+  </div>;
 };
