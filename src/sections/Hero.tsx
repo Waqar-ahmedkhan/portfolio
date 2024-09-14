9a126c7708b2a28fe469875494a634142a7e8f1a
Waqar ahmed <waqarahmed44870@gmail.com>
2024-09-14 13:50:09 +0500

    commit

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,8 +5,15 @@ import memoji from "../assets/images/memoji-computer.png"
   <div className="py-32 flex justify-center ">
      <div className="flex flex-col">
       <Image src={memoji} height={200} width={200}  alt="its profile image" className=""/>
-      <div>
-         <li className=""> the person avaiable here </li>
+  
+
+    
+      <div className="py-1 px-2  flex flex-row justify-evenly align-middle items-center gap-2 bg-gray-700 text-white border rounded-full">
+         <div className="h-3 w-3 rounded-full bg-green-500"></div>
+         <div className="">
+            im avaible to delivery your work
+
+         </div>
       </div>
      </div>
   </div>
