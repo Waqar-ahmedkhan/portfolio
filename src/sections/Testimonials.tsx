9283f419532c4a0f6a972adcebd31306476ea03d
Waqar ahmed <waqarahmed44870@gmail.com>
2024-10-11 21:42:48 +0500

    commit

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -5,6 +5,7 @@ export const TestimonialsSection = () => {
   return (
     <section className="bg-white h-screen dark:bg-gray-900 py-12">
       <div className="text-center mb-12">
+      <h2 className="text-teal-400 text-sm uppercase tracking-wide mb-2">Happy clients</h2>
         <h2 className="text-4xl font-bold text-black dark:text-white ">What Clients Say about Me</h2>
         <p className="text-gray-400 dark:text-gray-300 mt-4">Don&apos;t just take my word for it. See what my clients have to say about my work.</p>
       </div>
